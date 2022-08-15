e304b466f773f5598c1e93401789289a7126a791
Rody Davis <rody.davis.jr@gmail.com>
2022-08-15 14:45:20 -0700

    add base href

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://rodydavis.github.io/",
+  base: "/astro-playground-elements",
+});
